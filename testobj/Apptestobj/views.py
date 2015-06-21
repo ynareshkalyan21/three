11dba18e041b61a11b49d7835239aba1ba048656
kishore <kishore25kumar@gmail.com>
2015-06-21 18:56:42 +0530

    added static html added

--- testobj/Apptestobj/views.py
+++ testobj/Apptestobj/views.py
@@ -9,3 +9,39 @@ def home(request):
 def editor(request):
     context = {'msg':'Customize','path':request.path}
     return  render(request,'Apptestobj/editor.html',context)
+
+
+def step1(request):
+    return render(request, "Apptestobj/1.htm", {})
+
+
+def step2(request):
+    return render(request, "Apptestobj/2.htm", {})
+
+
+def step3(request):
+    return render(request, "Apptestobj/3.htm", {})
+
+
+def step4(request):
+    return render(request, "Apptestobj/4.htm", {})
+
+
+def step5(request):
+    return render(request, "Apptestobj/5.htm", {})
+
+
+def step6(request):
+    return render(request, "Apptestobj/6.htm", {})
+
+
+def step7(request):
+    return render(request, "Apptestobj/7.htm", {})
+
+
+def step8(request):
+    return render(request, "Apptestobj/8.htm", {})
+
+
+def step9(request):
+    return render(request, "Apptestobj/9.htm", {})
--- testobj/testobj/urls.py
+++ testobj/testobj/urls.py
@@ -16,4 +16,13 @@ urlpatterns = patterns('',
 
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
+                       url(r'^step1', "Apptestobj.views.step1", name="step1"),
+                       url(r'^step2', "Apptestobj.views.step2", name="step2"),
+                       url(r'^step3', "Apptestobj.views.step3", name="step3"),
+                       url(r'^step4', "Apptestobj.views.step4", name="step4"),
+                       url(r'^step5', "Apptestobj.views.step5", name="step5"),
+                       url(r'^step6', "Apptestobj.views.step6", name="step6"),
+                       url(r'^step7', "Apptestobj.views.step7", name="step7"),
+                       url(r'^step8', "Apptestobj.views.step8", name="step8"),
+                       url(r'^step9', "Apptestobj.views.step9", name="step9"),
 )
